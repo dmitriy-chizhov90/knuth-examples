4cc9ce222c0fe86acfafa59fdfe2815c3e1df139
Dmitriy Chizhov <dim-ch@yandex-team.ru>
2022-09-30 20:41:55 +0300

    ex 1.2.2 #23

--- ex_1_2_2.py
+++ ex_1_2_2.py
@@ -2,6 +2,7 @@
 
 import math
 import numpy as np
+import matplotlib.pyplot as plt
 from tabulate import tabulate
 from random import randrange
 
@@ -139,13 +140,143 @@ def bits_count(n):
     d = digits_count(n)
     return math.ceil(d*10/3)
 
-numbers = [randrange(1000000) for i in range(100)]
-numbers = sorted(numbers + [1, 9, 10, 11, 99, 100, 101, 999, 1000, 1001])
+def print_bits_count():
+    numbers = [randrange(1000000) for i in range(100)]
+    numbers = sorted(numbers + [1, 9, 10, 11, 99, 100, 101, 999, 1000, 1001])
 
-for n in numbers:
-    bits = bits_count(n)
-    upper = int(math.pow(2, bits))
-    lower = int(math.pow(2, bits-1))
-    print(f'{n}: {digits_count(n)} {bits}, [{lower}; {upper}]')
+    for n in numbers:
+        bits = bits_count(n)
+        upper = int(math.pow(2, bits))
+        lower = int(math.pow(2, bits-1))
+        print(f'{n}: {digits_count(n)} {bits}, [{lower}; {upper}]')
 
+# 23.
+
+def drawPlot(d, vlines=[], hlines=[], xtext=[], ytext=[], dots=[]):
+    plt.margins(x=0, y=0)
+
+    for l in vlines:
+        plt.axvline(x = l, color = '#000000', alpha=0.1)
+
+    for l in hlines:
+        plt.axhline(y = l, color = '#000000', alpha=0.1)
+
+    for x,txt in xtext:
+        plt.text(x, -0.1, txt, va='top', ha='center')
+    for y,txt in ytext:
+        plt.text(-0.1, y, txt, va='center', ha='right')
+
+    d()
+
+    for x,y in dots:
+        plt.plot(x,y,'ro')
+
+    plt.legend()
+    plt.show()
+
+def stretch_along_y(i, k1, k2, t0,shift=None):
+    shift = t0 if shift is None else shift
+    return ((k1-t0)*(i-shift)/(k2-t0))+t0
+    
+def printHiper():
+    x, y = 4., 25.
+    xy = x*y
+    t0 = 1.
+    _y = xy-y
+    _x = xy-x
+
+    hiper = np.vectorize(lambda i: 1./i)
+    hiper_div_x = np.vectorize(lambda i: 1./i/x)
+    hiper_str_x = np.vectorize(lambda i: 1./stretch_along_y(i,y,_x+t0,t0,x)/x)
+    hiper_div_y = np.vectorize(lambda i: 1./i/y)
+    hiper_str_y = np.vectorize(lambda i: 1./stretch_along_y(i,x,_y+t0,t0,y)/y)
+    ln = np.vectorize(lambda i: math.log(i))
+    ln_div_x = np.vectorize(lambda i: math.log(i)/x)
+    ln_str_x = np.vectorize(lambda i: math.log(stretch_along_y(i,y,xy,t0)))
+    ln_div_y = np.vectorize(lambda i: math.log(i)/y)
+    ln_str_y = np.vectorize(lambda i: math.log(stretch_along_y(i,x,xy,t0)))
+
+    step = 0.001
+    i_y = np.arange(t0, y+step, step)
+    i_x = np.arange(t0, x+step, step)
+    i_xy = np.arange(t0, xy+step, step)
+    i_76 = np.arange(t0, _y+t0+step, step) #[1..76]
+    i_97 = np.arange(t0, _x+t0+step, step) #[1..97]
+    i_25 = np.arange(y, xy+step, step) #[25..100]
+    i_4 = np.arange(x, xy+step, step) #[4..100]
+    i_all = np.arange(step, xy+step, step)
+
+    hiper_str = r'$\frac{1}{i}$'
+    ln_str = r'$\ln{i}$'
+    ln_div_x_str = r'$\frac{\ln{i}}{x}$'
+    ln_div_y_str = r'$\frac{\ln{i}}{y}$'
+    ln_str_x_str = r'$\ln{\frac{y-t_0}{xy-t_0}(i-t_0)+t_0}$'
+    ln_str_y_str = r'$\ln{\frac{x-t_0}{xy-t_0}(i-t_0)+t_0}$'
+    s_y_str = r'$S [1..y] \frac{1}{i}$'
+    s_x_str = r'$S [1..x] \frac{1}{i}$'
+    s_y_div_x_str = r'$S [1..y] \frac{1}{x i}$'
+    s_x_div_y_str = r'$S [1..x] \frac{1}{y i}$'
+    hiper_str_x_str = r'$S [x..xy] \frac{1}{x (\frac{i-x}{x}+t_0)}$'
+    hiper_str_y_str = r'$S [y..xy] \frac{1}{y (\frac{i-y}{y}+t_0)}$'
+    ln_sum_str=r'$\ln{(\frac{x-t_0}{xy-t_0}(i-t_0)+t_0)}+\ln{(\frac{y-t_0}{xy-t_0}(i-t_0)+t_0)}$'
+    
+    def drHiper(): plt.plot(i_xy, hiper(i_xy), 'b-', label=hiper_str)
+    def drLn(i=i_xy): plt.plot(i, ln(i), 'r-', label=ln_str)
+    def drLnDivX(): plt.plot(i_y, ln_div_x(i_y), 'm-', label=ln_div_x_str)
+    def drLnDivY(): plt.plot(i_x, ln_div_y(i_x), 'c-', label=ln_div_y_str)
+    def drLnStrX(): plt.plot(i_xy, ln_str_x(i_xy), 'c-', label=ln_str_x_str)
+    def drLnStrY(): plt.plot(i_xy, ln_str_y(i_xy), 'm-', label=ln_str_y_str)
+    def drHiperY(c='c'): plt.fill_between(i_y, hiper(i_y), color=c, label=s_y_str)
+    def drHiperX(c='m'): plt.fill_between(i_x, hiper(i_x), color=c, label=s_x_str)
+    def drHiperStrX(): plt.fill_between(i_4,hiper_str_x(i_4),color='c',label=hiper_str_x_str)
+    def drHiperStrY(): plt.fill_between(i_25,hiper_str_y(i_25),color='m',label=hiper_str_y_str)
+    def drHiperDivX(): plt.fill_between(i_y, hiper_div_x(i_y),color='m',label=s_y_div_x_str)
+    def drHiperDivY(): plt.fill_between(i_x, hiper_div_y(i_x),color='c',label=s_x_div_y_str)
+    def drLnSum(): plt.plot(i_xy,ln_str_x(i_xy)+ln_str_y(i_xy),'r--',label=ln_sum_str)
+
+    # Гипербола и логарифм
+    drawPlot(
+        lambda: (drHiper(),drLn(),drHiperY(),drHiperX()),
+        vlines=[x,y],
+        hlines=[math.log(x),math.log(y),math.log(xy)],
+        xtext=[(x,str((int(x)))),(y,str(int(y)))],
+        ytext=[
+            (math.log(x),str(r'$\ln{x}$')),
+            (math.log(y),r'$\ln{y}$'),
+            (math.log(xy),r'$\ln{xy}$')],
+        dots=[(x,math.log(x)),(y,math.log(y)),(xy,math.log(xy))]
+    )
+
+    # Логарифм x'
+    drawPlot(
+        lambda:(drHiper(),drHiperX('r'),drHiperDivY(),drHiperStrY()),
+        vlines=[x],
+        hlines=[math.log(x)/y],
+        xtext=[(x,str(int(x))), (y,str(int(y)))],
+        ytext=[(math.log(x)/y,r'$\frac{\ln{x}}{y}$')]
+    )
+
+    # Логарифм y'
+    drawPlot(
+        lambda:(drHiper(),drHiperStrX(), drHiperDivX()),
+        vlines=[y],
+        hlines=[math.log(y)/x],
+        xtext=[(x,str(int(x))), (y,str(int(y)))],
+        ytext=[(math.log(y)/x,r'$\frac{\ln{y}}{x}$')]
+    )
+    
+    # Логарифм x*y
+    drawPlot(
+        lambda:(drLn(),drLnStrX(),drLnStrY(),drLnSum()),
+        vlines=[x,y],
+        hlines=[math.log(x), math.log(y)],
+        xtext=[(x,str(int(x))),(y,str(int(y)))],
+        ytext=[
+            (math.log(x),r'$\ln{x}$'),
+            (math.log(y),r'$\ln{y}$'),
+        ],
+        dots=[(x,math.log(x)),(y,math.log(y)),(xy,math.log(xy))]
+    )
+        
+printHiper()
 
