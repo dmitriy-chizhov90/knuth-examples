53ec2ae4c11db1f15d855a07e00dd4dfdf34b176
Dmitriy Chizhov <dim-ch@yandex-team.ru>
2022-10-16 19:24:14 +0300

    ex 1.2.3 #1-6

--- ex_1_2_3.py
+++ ex_1_2_3.py
@@ -2,6 +2,15 @@
 
 import math
 
+# 2.
+def sum_2_1():
+    s=0
+    for i in range(6):
+       s+= 1./(2*i+1)
+    return s
+
+print(sum_2_1(), sum_2_1() - 6508./3465)
+
 def mult_26_1(n, af):
     p = 1
     for i in range(n + 1):
@@ -27,7 +36,7 @@ def mult_26_2(n, af):
 
     return math.pow(p, n + 2)
 
-print()
-for n in range(5):
-    print('{_n}, {_r}'.format(_n=n, _r=('pass' if mult_26_1(n, a1) == mult_26_2(n, a1) else 'fail')))
+#print()
+#for n in range(5):
+#    print('{_n}, {_r}'.format(_n=n, _r=('pass' if mult_26_1(n, a1) == mult_26_2(n, a1) else 'fail')))
 
