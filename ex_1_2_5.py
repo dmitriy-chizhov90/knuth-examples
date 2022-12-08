35ad2b50b652d617319e1ad8f99fc0c62f6e2e7b
Dmitriy Chizhov <dmitriy.chizhov90@gmail.com>
2022-12-08 23:33:55 +0300

    some partials

--- ex_1_2_5.py
+++ ex_1_2_5.py
@@ -17,7 +17,27 @@ def printFractPowers():
     utils.printExp('math.pow(10, 0.60464)', 4.023833481595328)
 
 # 5.
-utils.printExp('math.sqrt(2.*math.pi*8.)*math.pow((8./math.e), 8.)*(1. + 1./(12.*8.))', 40318.045405288554)
-utils.printExp('math.factorial(8)', 40320)
-utils.printExp('(40320 - 40318.045405288554)/40320*100', 0.004847705137514086)
+def printFactorialApprox():
+    utils.printExp('math.sqrt(2.*math.pi*8.)*math.pow((8./math.e), 8.)*(1. + 1./(12.*8.))', 40318.045405288554)
+    utils.printExp('math.factorial(8)', 40320)
+    utils.printExp('(40320 - 40318.045405288554)/40320*100', 0.004847705137514086)
 
+# 14.                                                                                                                                                                        
+# Факториалы для взаимнопростых чисел, меньших p.                                                                                                                            
+def print_orth_factorial(a, p):
+    f = 1
+    for i in range(1, a+1):
+  m = 1
+        for j in range(1, p):
+            m *= (i*p+j)
+  f *= m
+        print(f'        {i}: {m%p}, {f%p}')
+
+def print_orth_factorials():
+    for p in [2, 3, 5, 7, 11, 13]:
+        print(f'p = {p}:')
+  for i in range(1, p):
+            print(f'    p = {p}, a = {i}:')
+            print_orth_factorial(i, p)
+
+print_orth_factorials()
